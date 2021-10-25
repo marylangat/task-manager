36501c8eeeb9939438adc20407d6a063238338e4
Marymagdalene <chemtai.langat92@gmail.com>
2021-10-25 13:07:40 -0400

    initial upload

---
+++ app/secrets.js
@@ -0,0 +1,6 @@
+const username  = 'Mary'
+const password  = 'BZxn35BzacSsb.!'
+const dbName    = 'budgettracker'
+
+module.exports = {
+    'url' : `mongodb+srv://${username}:${password}@taskmanagercluster.aqyy0.mongodb.net/${dbName}?retryWrites=true&w=majority`};
